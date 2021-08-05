0dfb18d033d1c2d1713be46f8ded5cdf17211e07
samet bugra oktay <sbokty@gmail.com>
2021-08-05 21:45:56 +0300

    Initial Commit

---
+++ VeriparkApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VeriparkApp
+//
+//  Created by Samet Bugra Oktay on 5.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
